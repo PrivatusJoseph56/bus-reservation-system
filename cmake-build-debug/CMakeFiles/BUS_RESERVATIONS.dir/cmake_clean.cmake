2fd7c0a7ac1267105e333681fc47c4172ebf421e
PrivatusJoseph56 <privatus2004@gmail.com>
2024-12-23 21:54:52 +0300

    changed

---
+++ cmake-build-debug/CMakeFiles/BUS_RESERVATIONS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BUS_RESERVATIONS.exe"
+  "BUS_RESERVATIONS.exe.manifest"
+  "BUS_RESERVATIONS.pdb"
+  "CMakeFiles/BUS_RESERVATIONS.dir/main.cpp.obj"
+  "CMakeFiles/BUS_RESERVATIONS.dir/main.cpp.obj.d"
+  "libBUS_RESERVATIONS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BUS_RESERVATIONS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BUS_RESERVATIONS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BUS_RESERVATIONS.
+# This may be replaced when dependencies are built.
